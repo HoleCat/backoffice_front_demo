ca01010aae3e20410a2f747e0dfbd7a0679885e8
JorgeMateriaGris <94183198+JorgeMateriaGris@users.noreply.github.com>
2022-04-26 18:17:22 -0500

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './core/interfaces/User';
 //import { OverlayContainer } from '@angular/cdk/overlay';
 //import { HostBinding } from '@angular/core';
 
@@ -7,8 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   isCollapsed = false;
+
+  user:User = {
+    id: 0,
+    created_at: undefined,
+    created_by: 0,
+    document_number: '',
+    document_type: '',
+    email: '',
+    id_status: 0,
+    last_name: '',
+    name: '',
+    password: '',
+    phone: '',
+    photo: '',
+    updated_at: undefined,
+    updated_by: 0,
+    user_name: '',
+    id_document_type: 0
+  }
+
+  ngOnInit(): void {
+    sessionStorage.setItem('user', user);
+  }
   /*@HostBinding('class') componentCssClass: any;
   constructor(public overlayContainer: OverlayContainer){}
   public onSetTheme(e: string){
---
+++ src/app/features/chat/client-messages/client-messages.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientMessagesComponent } from './client-messages.component';
+
+describe('ClientMessagesComponent', () => {
+  let component: ClientMessagesComponent;
+  let fixture: ComponentFixture<ClientMessagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ClientMessagesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientMessagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
